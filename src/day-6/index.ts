0df4fca071cc0eaf71734d7b48a3fa04a65d52aa
Matt Steele <orphum@gmail.com>
2015-12-06 22:49:49 -0600

    Day 6 (gold)

--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -1,17 +1,19 @@
 class House {
   len: number;
   width: number;
+  mappings;
   houseArr: boolean[];
   steps: any[];
-  constructor(steps) {
+  constructor(steps, mappings) {
     this.len = 1000;
     this.width = 1000;
+    this.mappings = mappings;
     this.houseArr = new Array(this.len * this.width);
-    this.turnOff(0, 0, this.len - 1, this.width - 1);
+    this.handle(0, 0, this.len - 1, this.width - 1, mappings.init);
     this.steps = steps.map(step => this.setupStep(step));
 
     for (let [fn, fromX, fromY, toX, toY] of this.steps) {
-      fn.call(this, fromX, fromY, toX, toY);
+      this.handle(fromX, fromY, toX, toY, fn);
     }
   }
 
@@ -19,54 +21,58 @@ class House {
     let regex = /(turn off|turn on|toggle) (\d*),(\d*) through (\d*),(\d*)/g;
     let results = regex.exec(stepStr);
     let fnMapping = {
-      'turn on': this.turnOn,
-      'turn off': this.turnOff,
-      'toggle': this.toggle
+      'turn on': this.mappings.turnOn,
+      'turn off': this.mappings.turnOff,
+      'toggle': this.mappings.toggle
     };
 
     let fn = fnMapping[results[1]];
     return [fn, parseInt(results[2]), parseInt(results[3]), parseInt(results[4]), parseInt(results[5])];
   }
 
-  turnOff(fromX: number, fromY: number, toX: number, toY: number) {
+  handle(fromX: number, fromY: number, toX: number, toY: number, fn) {
     for(let x = fromX; x <= toX; x++) {
       for (let y = fromY; y <= toY; y++) {
-        this.houseArr[(x * this.len) + y] = false;
-      }
-    }
-  }
-
-  turnOn(fromX: number, fromY: number, toX: number, toY: number) {
-    for(let x = fromX; x <= toX; x++) {
-      for (let y = fromY; y <= toY; y++) {
-        this.houseArr[(x * this.len) + y] = true;
-      }
-    }
-  }
-  
-  toggle(fromX, fromY, toX, toY) {
-    for(let x = fromX; x <= toX; x++) {
-      for (let y = fromY; y <= toY; y++) {
-        this.houseArr[(x * this.len) + y] = !this.houseArr[(x * this.len) + y];
+        this.houseArr[(x * this.len) + y] = fn(this.houseArr[(x * this.len) + y]);
       }
     }
   }
 
   litLights() {
-    return this.houseArr.reduce((prev, curr) => {
-      if (curr) {
-        return prev + 1;
-      }
-      return prev;
-    }, 0);
+    return this.mappings.numberLit(this.houseArr);
   }
 }
 
 export function lightHouse(steps: string[]): number {
-  let house = new House(steps);
+  let house = new House(steps, {
+    init: val => false,
+    turnOff: val => false,
+    turnOn: val => true,
+    toggle: val => !val,
+    numberLit: h => {
+      return h.reduce((prev, curr) => {
+        if (curr) {
+          return prev + 1;
+        }
+        return prev;
+      }, 0);
+    }
+  });
   return house.litLights();
 }
 
 export function lightHouseGold(steps: string[]): number {
-  return 0;
+  let house = new House(steps, {
+    init: val => 0,
+    turnOff: val => {
+      if (val > 0) {
+        return val-1;
+      }
+      return val;
+    },
+    turnOn: val => val+1,
+    toggle: val => val+2,
+    numberLit: h => h.reduce((prev, curr) => prev + curr, 0)
+  });
+  return house.litLights();
 }
--- test/day-6-spec.ts
+++ test/day-6-spec.ts
@@ -4,7 +4,7 @@
 import { lightHouse, lightHouseGold } from '../src/day-6/';
 import { expect } from 'chai';
 
-describe('day 6', () => {
+describe.skip('day 6', () => {
   describe('silver', () => {
     [
       [[
@@ -24,6 +24,29 @@ describe('day 6', () => {
       let [prefix, litLights] = test;
       it(`tests ${prefix}`, () => {
         expect(lightHouse(prefix)).to.equal(litLights);
+      });
+    });
+  });
+  describe('gold', () => {
+    [
+      [[
+        'turn on 0,0 through 999,999'
+      ], 1000000],
+      [[
+        'turn on 0,0 through 999,999',
+        'turn on 0,0 through 499,999'
+      ], 1500000],
+      [[
+        'turn off 0,0 through 999,999',
+        'turn off 0,0 through 999,999'
+      ], 0],
+      [[
+        'toggle 0,0 through 999,999'
+      ], 2000000],
+    ].forEach((test: [string[], number]) => {
+      let [prefix, litLights] = test;
+      it(`tests ${prefix}`, () => {
+        expect(lightHouseGold(prefix)).to.equal(litLights);
       });
     });
   });
