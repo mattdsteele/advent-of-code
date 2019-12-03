9fdcfbaff0e8f2a0b1fb97a24d81df27bbe455da
mattdsteele <orphum@gmail.com>
2019-12-02 22:52:57 -0600

    Update fn

---
+++ deploy-fn.sh
@@ -0,0 +1,1 @@
+gcloud functions deploy Shields --runtime go111 --env-vars-file ./.env.local.yaml --trigger-http
--- shields.go
+++ shields.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/mattdsteele/advent-of-code/aoc"
 )
@@ -17,7 +16,6 @@ type Shield struct {
 
 func Shields(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	os.Setenv("AOC_SESSION_COOKIE", "53616c7465645f5f7e791b2ea86f758b14ce2d946ddcd09990a591b875cb1855a6498a5135e74fcd84ec5bf7c6798d77")
 	stars := aoc.Stars()
 	val := Shield{SchemaVersion: 1, Label: "advent-of-code", Message: fmt.Sprintf("%d stars", stars)}
 	j, _ := json.Marshal(val)
