f6031eb2cd2fa976ddec3a9b82a04bb100c0c267
mattdsteele <orphum@gmail.com>
2021-12-02 22:51:44 -0600

    Day 1 Silver

--- src/1/1_test.go
+++ src/1/1_test.go
@@ -4,8 +4,35 @@ import (
 	"testing"
 
 	tst "github.com/mattdsteele/advent-of-code/testing"
+	util "github.com/mattdsteele/advent-of-code"
 )
 
 func TestParse(t *testing.T) {
 	tst.Equals(t, 2, 2)
+}
+
+func TestIncremental(t *testing.T) {
+	data := `1
+2
+3
+4
+5`
+	lines := util.SliceAtLine(data)
+	val := IncrementalValues(lines)
+	tst.Equals(t, val, 4)
+
+
+}
+
+func TestUpDown(t *testing.T) {
+	data := `1
+2
+1
+4
+2`
+	lines := util.SliceAtLine(data)
+	val := IncrementalValues(lines)
+	tst.Equals(t, val, 2)
+
+
 }
--- src/1/code.go
+++ src/1/code.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	util "github.com/mattdsteele/advent-of-code"
+)
+
 func main() {
 	silver()
 }
 
+func IncrementalValues(input []string) int {
+	incremented := 0
+	var previous int
+	for _, level := range input {
+		cur, _ := strconv.Atoi(level)
+		if previous != 0 && previous < cur {
+			incremented++
+		}
+		previous = cur
+	}
+	return incremented
+}
+
 func silver() {
+	lines := util.ReadFile("./src/1/input.txt")
+	incremented := IncrementalValues(lines)
+	fmt.Println(incremented)
 
 }
+
