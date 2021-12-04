9e339b5c0a0d146035965c07265499864bbcd255
mattdsteele <orphum@gmail.com>
2021-12-04 15:49:11 -0600

    Day 1 Gold

--- src/1/1_test.go
+++ src/1/1_test.go
@@ -3,25 +3,40 @@ package main
 import (
 	"testing"
 
-	tst "github.com/mattdsteele/advent-of-code/testing"
 	util "github.com/mattdsteele/advent-of-code"
+	tst "github.com/mattdsteele/advent-of-code/testing"
 )
 
 func TestParse(t *testing.T) {
 	tst.Equals(t, 2, 2)
 }
 
-func TestIncremental(t *testing.T) {
+func TestBreakupLines(t *testing.T) {
 	data := `1
 2
 3
 4
 5`
 	lines := util.SliceAtLine(data)
-	val := IncrementalValues(lines)
-	tst.Equals(t, val, 4)
+	vals := Chunked(lines, 3)
+	tst.Equals(t, len(vals), 3)
+	tst.SlicesEqual([]int{1, 2, 3}, lines[0])
+	tst.SlicesEqual([]int{2, 3, 4}, lines[1])
+}
 
-
+func TestGoldExample(t *testing.T) {
+	data := `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`
+	gr := goldResult(util.SliceAtLine(data))
+	tst.Equals(t, 5, gr)
 }
 
 func TestUpDown(t *testing.T) {
@@ -33,6 +48,5 @@ func TestUpDown(t *testing.T) {
 	lines := util.SliceAtLine(data)
 	val := IncrementalValues(lines)
 	tst.Equals(t, val, 2)
-
 
 }
