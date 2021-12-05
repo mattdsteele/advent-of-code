12f5fc43345f140596db4e8e941ea9dadaf45b64
mattdsteele <orphum@gmail.com>
2021-12-04 20:47:19 -0600

    Day 2 Silver

--- src/2/2_test.go
+++ src/2/2_test.go
@@ -3,9 +3,26 @@ package main
 import (
 	"testing"
 
+	util "github.com/mattdsteele/advent-of-code"
 	tst "github.com/mattdsteele/advent-of-code/testing"
 )
 
 func TestParse(t *testing.T) {
-	tst.Equals(t, 2, 2)
+	sampleData := util.SliceAtLine(exampleData())
+	tst.Equals(t, 150, silverData(sampleData))
+}
+
+func TestParseLine(t *testing.T) {
+	dir, amt := parseLine("forward 5")
+	tst.Equals(t, "forward", dir)
+	tst.Equals(t, 5, amt)
+}
+
+func exampleData() string {
+	return `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`
 }
--- src/2/code.go
+++ src/2/code.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	util "github.com/mattdsteele/advent-of-code"
+)
 
 func main() {
 	silver()
@@ -8,8 +14,31 @@ func main() {
 }
 
 func silver() {
-	fmt.Println("Silver result")
+	lines := util.ReadFile("src/2/input.txt")
+	fmt.Println(silverData(lines))
+}
 
+func silverData(sampleData []string) int {
+	var horizontal, vertical int
+	for _, step := range sampleData {
+		direction, amt := parseLine(step)
+		switch direction {
+		case "forward":
+			horizontal += amt
+		case "up":
+			vertical -= amt
+		case "down":
+			vertical += amt
+		}
+	}
+	return horizontal * vertical
+}
+
+func parseLine(line string) (direction string, value int) {
+	values := strings.Split(line, " ")
+	direction = values[0]
+	value, _ = strconv.Atoi(values[1])
+	return direction, value
 }
 
 func gold() {
