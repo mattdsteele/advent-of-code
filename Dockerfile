e2462fd06c4215a831190daa3dcfd18189ed1712
mattdsteele <orphum@gmail.com>
2021-12-04 20:20:57 -0600

    Remove GH Actions

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM golang:latest
-
-RUN apt-get update && apt-get install -y ruby-full
-CMD cd $GITHUB_WORKSPACE && ruby ./cmd/sync/sync.rb
--- template/code.go
+++ template/code.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	silver()
+	gold()
 }
 
 func silver() {
+	fmt.Println("Silver result")
 
+}
+
+func gold() {
+	// Print gold result
 }
