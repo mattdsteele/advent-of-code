c6881f0a0ce810dbe53a7d406ea3fb2e2accd030
Matt Steele <orphum@gmail.com>
2022-12-09 04:27:51 +0000

    7 silver

---
+++ src/7/7_code.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	util "github.com/mattdsteele/advent-of-code"
+)
+
+func main() {
+	silver()
+	// gold()
+}
+
+func silver() {
+	lines := util.ReadFile("src/7/input.txt")
+	fmt.Println(silverCalculate(lines))
+}
+
+func silverCalculate(input []string) string {
+	total := 0
+	rootDir := parseInput(input)
+	s := rootDir.dirsUnder(100000)
+	for _, d := range s {
+		total += d.size()
+	}
+	return fmt.Sprint(total)
+}
+
+func gold() {
+	lines := util.ReadFile("src/7/input.txt")
+	fmt.Println(goldCalculate(lines))
+}
+
+func goldCalculate(lines []string) string {
+	return "input"
+}
+
+func parseInput(lines []string) *Directory {
+	rootDir := new(Directory)
+	var currentDir *Directory
+	for _, l := range lines {
+		if strings.Contains(l, "$ cd") {
+			newDir := l[5:]
+			if newDir == "/" {
+				currentDir = rootDir
+			} else if newDir == ".." {
+				currentDir = currentDir.parent
+			} else {
+				currentDir = currentDir.cd(newDir)
+			}
+		} else {
+			currentDir.add(l)
+		}
+	}
+	return rootDir
+}
+
+type Directory struct {
+	name        string
+	parent      *Directory
+	directories []*Directory
+	files       []*File
+}
+
+func (dir *Directory) dirsUnder(size int) (dirs []*Directory) {
+	flat := dir.flatDirs()
+	for _, d := range flat {
+		if d.size() < size {
+			dirs = append(dirs, d)
+		}
+	}
+	return dirs
+}
+func (dir *Directory) flatDirs() (dirs []*Directory) {
+	dirs = append(dirs, dir)
+	for _, d := range dir.directories {
+		dirs = append(dirs, d.flatDirs()...)
+	}
+	return dirs
+}
+
+func (dir *Directory) size() (s int) {
+	for _, f := range dir.files {
+		s += f.size
+	}
+	for _, d := range dir.directories {
+		s += d.size()
+	}
+	return s
+}
+func (dir *Directory) add(line string) {
+	s := strings.Split(line, " ")
+	if s[0] == "dir" {
+		newDir := new(Directory)
+		newDir.name = s[1]
+		newDir.parent = dir
+		dir.directories = append(dir.directories, newDir)
+	} else {
+		newFile := new(File)
+		size, _ := strconv.Atoi(s[0])
+		newFile.size = size
+		newFile.name = s[1]
+		dir.files = append(dir.files, newFile)
+	}
+}
+func (dir *Directory) cd(subfolder string) *Directory {
+	for _, d := range dir.directories {
+		if d.name == subfolder {
+			return d
+		}
+	}
+	panic("no known subfolder in dir")
+}
+
+type File struct {
+	name string
+	size int
+}
---
+++ src/7/7_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	util "github.com/mattdsteele/advent-of-code"
+	tst "github.com/mattdsteele/advent-of-code/testing"
+)
+
+func TestParse(t *testing.T) {
+	tst.Equals(t, 2, 2)
+	exampleInput := `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`
+	lines := util.SliceAtLine(exampleInput)
+	rootDir := parseInput(lines)
+	tst.Equals(t, 48381165, rootDir.size())
+	tst.Equals(t, "95437", silverCalculate(lines))
+}
