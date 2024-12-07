59ac2115d6d29dd2a5ba7c1fbc4a13ceaebffbcd
mattdsteele <orphum@gmail.com>
2024-12-06 20:57:49 -0600

    Day 2 gold

--- src/2/2_code.go
+++ src/2/2_code.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	silver()
-	// gold()
+	gold()
 }
 
 func silver() {
@@ -21,7 +21,7 @@ func silver() {
 func silverCalculate(input []string) string {
 	safeCount := 0
 	for _, l := range input {
-		if isSafe(l) {
+		if silverSafe(l) {
 			safeCount++
 		}
 	}
@@ -33,17 +33,54 @@ func gold() {
 	fmt.Println(goldCalculate(lines))
 }
 
-func goldCalculate(lines []string) string {
-	return "input"
+func goldCalculate(input []string) string {
+	safeCount := 0
+	for _, l := range input {
+		if goldSafe(l) {
+			safeCount++
+		}
+	}
+	return strconv.Itoa(safeCount)
 }
 
-func isSafe(line string) bool {
+func goldSafe(line string) bool {
+	numi := toIntSlice(line)
+	// Original value?
+	firstCheck := safeChecks(numi)
+	if firstCheck {
+		return true
+	}
+
+	// If not, remove each entry and perform a check
+	for i := range numi {
+		freshNum := toIntSlice(line)
+		if safeChecks(removeEntry(freshNum, i)) {
+			return true
+		}
+	}
+	return false
+}
+
+func removeEntry(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func silverSafe(line string) bool {
+	numi := toIntSlice(line)
+	return safeChecks(numi)
+}
+
+func toIntSlice(line string) []int {
 	nums := strings.Split(line, " ")
 	numi := []int{}
 	for _, num := range nums {
 		i, _ := strconv.Atoi(num)
 		numi = append(numi, i)
 	}
+	return numi
+}
+
+func safeChecks(numi []int) bool {
 	if alwaysDecreasing(numi) {
 		return true
 	}
--- src/2/2_test.go
+++ src/2/2_test.go
@@ -6,11 +6,20 @@ import (
 	tst "github.com/mattdsteele/advent-of-code/testing"
 )
 
-func TestLine1(t *testing.T) {
-	tst.Equals(t, true, isSafe("7 6 4 2 1"))
-	tst.Equals(t, true, isSafe("1 3 6 7 9"))
-	tst.Equals(t, false, isSafe("1 2 7 8 9"))
-	tst.Equals(t, false, isSafe("9 7 6 2 1"))
-	tst.Equals(t, false, isSafe("1 3 2 4 5"))
-	tst.Equals(t, false, isSafe("8 6 4 4 1"))
+func TestSilver(t *testing.T) {
+	tst.Equals(t, true, silverSafe("7 6 4 2 1"))
+	tst.Equals(t, true, silverSafe("1 3 6 7 9"))
+	tst.Equals(t, false, silverSafe("1 2 7 8 9"))
+	tst.Equals(t, false, silverSafe("9 7 6 2 1"))
+	tst.Equals(t, false, silverSafe("1 3 2 4 5"))
+	tst.Equals(t, false, silverSafe("8 6 4 4 1"))
+}
+
+func TestGold(t *testing.T) {
+	tst.Equals(t, true, goldSafe("7 6 4 2 1"))
+	tst.Equals(t, true, goldSafe("1 3 6 7 9"))
+	tst.Equals(t, false, goldSafe("1 2 7 8 9"))
+	tst.Equals(t, false, goldSafe("9 7 6 2 1"))
+	tst.Equals(t, true, goldSafe("1 3 2 4 5"))
+	tst.Equals(t, true, goldSafe("8 6 4 4 1"))
 }
