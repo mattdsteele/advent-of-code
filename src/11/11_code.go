253f437ef9dbe1c3f8d50da5becfff326e7fbfb6
mattdsteele <orphum@gmail.com>
2024-12-29 17:13:51 -0600

    11 Gold

--- src/11/11_code.go
+++ src/11/11_code.go
@@ -8,9 +8,11 @@ import (
 	util "github.com/mattdsteele/advent-of-code"
 )
 
+var results map[string]int = make(map[string]int)
+
 func main() {
 	silver()
-	// gold()
+	gold()
 }
 
 func silver() {
@@ -20,20 +22,31 @@ func silver() {
 
 func silverCalculate(input []string) string {
 	iterations := 25
+	count := 0
+
 	in := input[0]
-	for i := 0; i < iterations; i++ {
-		in = silverIterate(in)
+	for _, i := range strings.Split(in, " ") {
+		n, _ := strconv.Atoi(i)
+		count += iterate(1, n, iterations)
 	}
-	return strconv.Itoa(len(strings.Split(in, " ")))
+	return strconv.Itoa(count)
 }
 
 func gold() {
-	lines := util.ReadFile("src/X/input.txt")
+	lines := util.ReadFile("src/11/input.txt")
 	fmt.Println(goldCalculate(lines))
 }
 
-func goldCalculate(lines []string) string {
-	return "input"
+func goldCalculate(input []string) string {
+	iterations := 75
+	count := 0
+
+	in := input[0]
+	for _, i := range strings.Split(in, " ") {
+		n, _ := strconv.Atoi(i)
+		count += iterate(1, n, iterations)
+	}
+	return strconv.Itoa(count)
 }
 
 func silverIterate(exampleInput string) string {
@@ -59,4 +72,40 @@ func silverIterate(exampleInput string) string {
 
 	}
 	return strings.Join(vals, " ")
+}
+
+func iterate(count, n, iterationsRemaining int) int {
+	key := fmt.Sprintf("%d,%d", n, iterationsRemaining)
+	val := results[key]
+	if val > 0 {
+		return val
+	}
+
+	if iterationsRemaining == 0 {
+		results[key] = count
+		return count
+	}
+
+	iterationsRemaining--
+
+	if n == 0 {
+		c := iterate(count, 1, iterationsRemaining)
+		results[key] = c
+		return c
+	}
+
+	ns := strconv.Itoa(n)
+	if len(ns)%2 == 0 {
+		firstS := ns[0 : len(ns)/2]
+		secondS := ns[len(ns)/2:]
+		first, _ := strconv.Atoi(firstS)
+		second, _ := strconv.Atoi(secondS)
+		c := iterate(count, first, iterationsRemaining) + iterate(1, second, iterationsRemaining)
+		results[key] = c
+		return c
+	}
+
+	c := iterate(count, n*2024, iterationsRemaining)
+	results[key] = c
+	return c
 }
