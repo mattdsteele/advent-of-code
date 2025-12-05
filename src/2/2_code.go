801c04e503600b9857ec577dbef3dd13817bd48b
mattdsteele <orphum@gmail.com>
2025-12-04 22:35:05 -0600

    2 gold

--- src/2/2_code.go
+++ src/2/2_code.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	silver()
-	// gold()
+	gold()
 }
 
 func silver() {
@@ -49,11 +49,50 @@ func valid(input int) bool {
 	return first != second
 }
 
+func goldValid(input int) bool {
+	inputStr := strconv.Itoa(input)
+	for inputSize := 1; inputSize <= len(inputStr)/2; inputSize++ {
+		if len(inputStr)%inputSize != 0 {
+			continue
+		}
+
+		first := inputStr[0:inputSize]
+		allEqual := true
+		for i := 1; i < len(inputStr)/inputSize; i++ {
+			next := inputStr[i*inputSize : i*inputSize+inputSize]
+			if first != next {
+				allEqual = false
+				break
+			}
+		}
+		if allEqual {
+			return false
+		}
+	}
+	return true
+}
+
 func gold() {
 	lines := util.ReadFile("src/2/input.txt")
 	fmt.Println(goldCalculate(lines))
 }
 
-func goldCalculate(lines []string) string {
-	return "input"
+func goldCalculate(input []string) string {
+	line := input[0]
+	ranges := strings.Split(line, ",")
+	invalidCount := 0
+
+	for _, idRange := range ranges {
+		els := strings.Split(idRange, "-")
+		first, _ := strconv.Atoi(els[0])
+		last, _ := strconv.Atoi(els[1])
+		for i := first; i <= last; i++ {
+			if !goldValid(i) {
+				invalidCount += i
+			}
+
+		}
+	}
+
+	return strconv.Itoa(invalidCount)
 }
--- src/2/2_test.go
+++ src/2/2_test.go
@@ -12,6 +12,11 @@ func TestSilver(t *testing.T) {
 	lines := util.SliceAtLine(exampleInput)
 	tst.Equals(t, silverCalculate(lines), "1227775554")
 }
+func TestGold(t *testing.T) {
+	exampleInput := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+	lines := util.SliceAtLine(exampleInput)
+	tst.Equals(t, goldCalculate(lines), "4174379265")
+}
 
 func TestValid(t *testing.T) {
 	tst.Equals(t, valid(11), false)
@@ -19,4 +24,13 @@ func TestValid(t *testing.T) {
 	tst.Equals(t, valid(22), false)
 	tst.Equals(t, valid(1188511885), false)
 	tst.Equals(t, valid(1698522), true)
+}
+func TestGoldValid(t *testing.T) {
+	tst.Equals(t, goldValid(11), false)
+	tst.Equals(t, goldValid(12), true)
+	tst.Equals(t, goldValid(22), false)
+	tst.Equals(t, goldValid(99), false)
+	tst.Equals(t, goldValid(100), true)
+	tst.Equals(t, goldValid(111), false)
+	tst.Equals(t, goldValid(1010), false)
 }
