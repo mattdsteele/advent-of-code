788f62c554effa19b4661d3a24de5c02f09f2c20
mattdsteele <orphum@gmail.com>
2025-12-24 21:59:36 -0600

    6 gold

--- src/6/6_code.go
+++ src/6/6_code.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	silver()
-	// gold()
+	gold()
 }
 
 func silver() {
@@ -28,12 +28,65 @@ func gold() {
 }
 
 func goldCalculate(input []string) string {
-	return "input"
+	return fmt.Sprintf("%d", goldParse(input).gold())
 }
 
 type Math struct {
 	operations []string
 	data       [][]int
+	lines      []string
+	opLine     string
+}
+
+func goldParse(lines []string) *Math {
+	math := Math{}
+	math.operations = []string{}
+	for i, l := range lines {
+		if (i + 1) < len(lines) {
+			math.lines = append(math.lines, l)
+		} else {
+			math.opLine = l
+		}
+	}
+	return &math
+}
+
+func (m *Math) gold() int {
+	total := 0
+	i := len(m.opLine) - 1
+	data := []string{}
+	for i >= 0 {
+		val := ""
+		for _, l := range m.lines {
+			c := string(l[i])
+			if c != " " {
+				val += c
+			}
+		}
+		if val != "" {
+			data = append(data, val)
+		}
+
+		op := string(m.opLine[i])
+		if op != " " {
+			operator := getOperator(op)
+			total += goldCalc(data, operator)
+
+			// reset values
+			data = []string{}
+		}
+		i--
+	}
+	return total
+}
+
+func goldCalc(data []string, operator operator) int {
+	ints := []int{}
+	for _, d := range data {
+		i, _ := strconv.Atoi(d)
+		ints = append(ints, i)
+	}
+	return operator(ints)
 }
 
 func parse(lines []string) *Math {
--- src/6/6_test.go
+++ src/6/6_test.go
@@ -7,7 +7,7 @@ import (
 	tst "github.com/mattdsteele/advent-of-code/testing"
 )
 
-func TestParse(t *testing.T) {
+func TestSilver(t *testing.T) {
 	tst.Equals(t, 2, 2)
 	exampleInput := `123 328  51 64 
  45 64  387 23 
@@ -16,4 +16,14 @@ func TestParse(t *testing.T) {
 	lines := util.SliceAtLine(exampleInput)
 	data := parse(lines)
 	tst.Equals(t, 4277556, data.silver())
+}
+
+func TestGold(t *testing.T) {
+	exampleInput := `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `
+	lines := util.SliceAtLine(exampleInput)
+	data := goldParse(lines)
+	tst.Equals(t, 3263827, data.gold())
 }
