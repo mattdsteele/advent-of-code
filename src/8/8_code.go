94d3cd4b3552e15c07173553599f76192fe457c8
mattdsteele <orphum@gmail.com>
2025-12-29 11:39:08 -0600

    8 silver

---
+++ src/8/8_code.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+
+	util "github.com/mattdsteele/advent-of-code"
+)
+
+func main() {
+	silver()
+	// gold()
+}
+
+func silver() {
+	lines := util.ReadFile("src/8/input.txt")
+	fmt.Println(silverCalculate(lines))
+}
+
+func silverCalculate(input []string) string {
+	g := parse(input)
+	return fmt.Sprintf("%d", g.silver(1000))
+}
+
+func gold() {
+	lines := util.ReadFile("src/8/input.txt")
+	fmt.Println(goldCalculate(lines))
+}
+
+func goldCalculate(input []string) string {
+	return "input"
+}
+
+type Point struct {
+	x, y, z int
+}
+
+type Junctions struct {
+	points map[*Point]bool
+}
+
+type Game struct {
+	points      []*Point
+	comparisons []*Comparison
+	junctions   []*Junctions
+}
+
+type Comparison struct {
+	p1, p2   *Point
+	distance float64
+}
+
+func (g *Game) silver(connections int) int {
+	for i := 0; i < connections; i++ {
+		p1 := g.comparisons[i].p1
+		p2 := g.comparisons[i].p2
+
+		// check if p1 is in a set
+		// add to set
+
+		p1Junction := g.getJunction(p1)
+		p2Junction := g.getJunction(p2)
+		if p1Junction != nil && p2Junction != nil {
+
+			if p2Junction != p1Junction {
+				// merge sets
+				for k := range p2Junction.points {
+					p1Junction.points[k] = true
+				}
+
+				// delete original junction
+				for i, j := range g.junctions {
+					if j == p2Junction {
+						g.junctions = append(g.junctions[:i], g.junctions[i+1:]...)
+					}
+				}
+			} else {
+			}
+		} else if p1Junction == nil && p2Junction != nil {
+			p2Junction.points[p1] = true
+
+		} else if p2Junction == nil && p1Junction != nil {
+			p1Junction.points[p2] = true
+		} else {
+			j := Junctions{}
+			j.points = make(map[*Point]bool)
+			j.points[p1] = true
+			j.points[p2] = true
+			g.junctions = append(g.junctions, &j)
+		}
+	}
+
+	// now show the 3 largest circuits
+	sort.Slice(g.junctions, func(p1, p2 int) bool {
+		return len(g.junctions[p1].points) > len(g.junctions[p2].points)
+	})
+	items := 3
+	total := 1
+	for i := 0; i < items; i++ {
+		size := len(g.junctions[i].points)
+		total *= size
+	}
+	return total
+}
+
+func (g *Game) getJunction(p1 *Point) *Junctions {
+	for _, j := range g.junctions {
+		if j.points[p1] == true {
+			return j
+		}
+	}
+	return nil
+}
+
+func parse(lines []string) *Game {
+	g := Game{}
+	g.points = []*Point{}
+	for _, l := range lines {
+		g.points = append(g.points, parseLine(l))
+	}
+
+	for _, p1 := range g.points {
+		for _, p2 := range g.points {
+			g.comparisons = append(g.comparisons, &Comparison{p1, p2, distance(p1, p2)})
+		}
+	}
+
+	sort.Slice(g.comparisons, func(p1, p2 int) bool {
+		return g.comparisons[p1].distance < g.comparisons[p2].distance
+	})
+
+	// remove dupes
+	filteredComparisons := []*Comparison{}
+	for i, c := range g.comparisons {
+		if i%2 == 0 && c.distance > 0 {
+			filteredComparisons = append(filteredComparisons, c)
+		}
+	}
+	g.comparisons = filteredComparisons
+
+	return &g
+}
+
+func parseLine(line string) *Point {
+	pts := strings.Split(line, ",")
+	xStr, yStr, zStr := pts[0], pts[1], pts[2]
+	x, _ := strconv.Atoi(xStr)
+	y, _ := strconv.Atoi(yStr)
+	z, _ := strconv.Atoi(zStr)
+	return &Point{x, y, z}
+}
+
+func distance(p1, p2 *Point) float64 {
+	return math.Sqrt(math.Pow(float64(p2.x-p1.x), 2) + math.Pow(float64(p2.y-p1.y), 2) + math.Pow(float64(p2.z-p1.z), 2))
+}
---
+++ src/8/8_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"math"
+	"testing"
+
+	util "github.com/mattdsteele/advent-of-code"
+	tst "github.com/mattdsteele/advent-of-code/testing"
+)
+
+var sample = `162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689`
+
+var lines = util.SliceAtLine(sample)
+
+func TestSilver(t *testing.T) {
+	tst.Equals(t, math.Sqrt(48), distance(&Point{0, 0, 0}, &Point{4, 4, 4}))
+	g := parse(lines)
+	tst.Equals(t, 190, len(g.comparisons))
+
+	// first value
+	tst.Equals(t, g.comparisons[0].p1.x, 162)
+	tst.Equals(t, g.comparisons[0].p2.x, 425)
+
+	tst.Equals(t, g.comparisons[1].p1.x, 431)
+	tst.Equals(t, g.comparisons[1].p2.x, 162)
+
+	tst.Equals(t, 40, g.silver(10))
+}
