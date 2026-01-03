7c717a42492a1c6a223a34ee0c6a7326d258138b
mattdsteele <orphum@gmail.com>
2026-01-02 21:22:30 -0600

    9 gold, hope it works

--- src/9/9_code.go
+++ src/9/9_code.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	silver()
-	// gold()
+	gold()
 }
 
 func silver() {
@@ -27,12 +27,145 @@ func gold() {
 	fmt.Println(goldCalculate(lines))
 }
 
-func goldCalculate(lines []string) string {
-	return "input"
+func goldCalculate(lines []string) int {
+	return parse(lines).gold()
 }
 
 type Game struct {
-	points [][]int
+	points   [][]int
+	segments []*Segment
+}
+
+// https://wrfranklin.org/Research/Short_Notes/pnpoly.html
+func (g *Game) pointIsInside(x, y int) bool {
+	isInside := false
+	for _, s := range g.segments {
+		i := s.point1
+		j := s.point2
+		if (i.y > y) != (j.y > y) &&
+			x < (j.x-i.x)*(y-i.y)/(j.y-i.y)+i.x {
+			isInside = !isInside
+		}
+	}
+	return isInside
+}
+
+func between(p, a, b int) bool {
+	return p >= a && p <= b || p <= a && p >= b
+}
+
+func (g *Game) pointIsInside2(x, y int) bool {
+	inside := false
+	P := &Point{x, y}
+	for _, s := range g.segments {
+		A := s.point1
+		B := s.point2
+
+		if P.x == A.x && P.y == A.y || P.x == B.x && P.y == B.y {
+			return true
+		}
+		if A.y == B.y && P.y == A.y && between(P.x, A.x, B.x) {
+			return true
+		}
+
+		if between(P.y, A.y, B.y) { // if P inside the vertical range
+			// filter out "ray pass vertex" problem by treating the line a little lower
+			if P.y == A.y && B.y >= A.y || P.y == B.y && A.y >= B.y {
+				continue
+			}
+			// calc cross product `PA X PB`, P lays on left side of AB if c > 0
+			c := (A.x-P.x)*(B.y-P.y) - (B.x-P.x)*(A.y-P.y)
+			if c == 0 {
+				return true
+			}
+			if (A.y < B.y) == (c > 0) {
+				inside = !inside
+			}
+		}
+	}
+	return inside
+}
+
+/*
+// Source - https://stackoverflow.com/a
+// Posted by timepp, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-01-02, License - CC BY-SA 4.0
+
+* Get relationship between a point and a polygon using ray-casting algorithm
+ * @param {{x:number, y:number}} P: point to check
+ * @param {{x:number, y:number}[]} polygon: the polygon
+ * @returns -1: outside, 0: on edge, 1: inside
+function relationPP(P, polygon) {
+    const between = (p, a, b) => p >= a && p <= b || p <= a && p >= b
+    let inside = false
+    for (let i = polygon.length-1, j = 0; j < polygon.length; i = j, j++) {
+        const A = polygon[i]
+        const B = polygon[j]
+        // corner cases
+        if (P.x == A.x && P.y == A.y || P.x == B.x && P.y == B.y) return 0
+        if (A.y == B.y && P.y == A.y && between(P.x, A.x, B.x)) return 0
+
+        if (between(P.y, A.y, B.y)) { // if P inside the vertical range
+            // filter out "ray pass vertex" problem by treating the line a little lower
+            if (P.y == A.y && B.y >= A.y || P.y == B.y && A.y >= B.y) continue
+            // calc cross product `PA X PB`, P lays on left side of AB if c > 0
+            const c = (A.x - P.x) * (B.y - P.y) - (B.x - P.x) * (A.y - P.y)
+            if (c == 0) return 0
+            if ((A.y < B.y) == (c > 0)) inside = !inside
+        }
+    }
+
+    return inside? 1 : -1
+}
+
+*/
+
+func (g *Game) isInside(ix, iy, jx, jy int) bool {
+	top := Segment{&Point{ix, iy}, &Point{ix, jy}}
+	right := Segment{&Point{ix, jy}, &Point{jx, jy}}
+	bottom := Segment{&Point{jx, jy}, &Point{jx, iy}}
+	left := Segment{&Point{jx, iy}, &Point{ix, iy}}
+	box := []*Segment{&top, &right, &bottom, &left}
+	for _, bs := range box {
+		for _, gs := range g.segments {
+			if segmentsIntersect(bs, gs) {
+				return false
+			}
+		}
+	}
+
+	// check each corner
+	for _, bs := range box {
+		p := bs.point1
+		if !g.pointIsInside2(p.x, p.y) {
+			return false
+		}
+	}
+
+	// check one inside
+	minX := min(ix, jx)
+	minY := min(iy, jy)
+	if !g.pointIsInside2(minX+1, minY+1) {
+		return false
+	}
+
+	return true
+}
+
+func (g *Game) gold() int {
+	max := 0
+	for _, i := range g.points {
+		for _, j := range g.points {
+			a := area(i, j)
+			if a > 0 {
+				if g.isInside(i[0], i[1], j[0], j[1]) && a > max {
+					// fmt.Printf("new max (%d): %d,%d - %d,%d\n", a, i[0], i[1], j[0], j[1])
+					max = a
+				}
+			}
+		}
+	}
+	return max
 }
 
 func parse(lines []string) *Game {
@@ -44,6 +177,19 @@ func parse(lines []string) *Game {
 		yInt, _ := strconv.Atoi(yStr)
 		g.points = append(g.points, []int{xInt, yInt})
 	}
+
+	// create segments
+	var prevPoint *Point
+	for _, p := range g.points {
+		currPoint := &Point{p[0], p[1]}
+		if prevPoint != nil {
+			g.segments = append(g.segments, &Segment{prevPoint, currPoint})
+		}
+		prevPoint = currPoint
+	}
+	// wrap around
+	first := g.points[0]
+	g.segments = append(g.segments, &Segment{prevPoint, &Point{first[0], first[1]}})
 	return &g
 }
 func (g *Game) silver() int {
@@ -69,4 +215,80 @@ func abs(x, y int) int {
 		diff = diff * -1
 	}
 	return diff
+}
+
+type Point struct {
+	x, y int
+}
+
+type Segment struct {
+	point1 *Point
+	point2 *Point
+}
+
+func (s *Segment) minX() int {
+	return min(s.point1.x, s.point2.x)
+}
+func (s *Segment) maxX() int {
+	return max(s.point1.x, s.point2.x)
+}
+func (s *Segment) minY() int {
+	return min(s.point1.y, s.point2.y)
+}
+func (s *Segment) maxY() int {
+	return max(s.point1.y, s.point2.y)
+}
+
+func (s *Segment) isVertical() bool {
+	return s.point1.x == s.point2.x
+}
+
+func segmentsIntersect(s1 *Segment, s2 *Segment) bool {
+	if s1.isVertical() == s2.isVertical() {
+		// both are same direction, they do not intersect
+		return false
+	}
+	// 2,0 - 2,2
+	// 1,1 - 3,1
+	// if there is overlap, they intersect
+
+	// x overlap scenario 1
+	if s1.maxX() > s2.minX() && s1.minX() < s2.maxX() {
+		if s1.maxY() > s2.minY() && s1.minY() < s2.maxY() {
+			// y overlap scenario 1
+			return true
+		}
+
+		if s2.maxY() > s1.minY() && s2.minY() < s1.maxY() {
+			// y overlap scenario 2
+			return true
+		}
+	}
+
+	// x overlap scenario 1
+	if s2.maxX() > s1.minX() && s2.minX() < s1.maxX() {
+		if s1.maxY() > s2.minY() && s1.minY() < s2.maxY() {
+			// y overlap scenario 1
+			return true
+		}
+
+		if s2.maxY() > s1.minY() && s2.minY() < s1.maxY() {
+			// y overlap scenario 2
+			return true
+		}
+
+	}
+	return false
+}
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
 }
--- src/9/9_test.go
+++ src/9/9_test.go
@@ -21,3 +21,52 @@ func TestParse(t *testing.T) {
 	p := parse(lines)
 	tst.Equals(t, 50, p.silver())
 }
+
+func TestGold(t *testing.T) {
+	lines := util.SliceAtLine(sampleData)
+	p := parse(lines)
+	tst.Equals(t, false, p.isInside(11, 1, 4, 4))
+	tst.Equals(t, true, p.isInside(11, 1, 7, 3))
+	tst.Equals(t, false, p.isInside(11, 1, 2, 3))
+
+	tst.Equals(t, true, p.isInside(2, 5, 7, 3))
+	tst.Equals(t, false, p.isInside(2, 5, 7, 1))
+
+	tst.Equals(t, false, p.isInside(2, 5, 9, 7))
+	tst.Equals(t, true, p.isInside(9, 5, 11, 7))
+
+	tst.Equals(t, true, p.pointIsInside2(11, 7))
+	tst.Equals(t, 24, p.gold())
+}
+
+func TestIntersects(t *testing.T) {
+	// both same direction
+	assertIntersects(t, 0, 2, 2, 2, 0, 1, 1, 1, false)
+	assertIntersects(t, 2, 0, 2, 2, 1, 1, 1, 3, false)
+
+	// overlaps
+	assertIntersects(t, 2, 0, 2, 2, 1, 1, 3, 1, true)
+	assertIntersects(t, 2, 0, 2, 2, 3, 1, 1, 1, true)
+	assertIntersects(t, 2, 2, 2, 0, 3, 1, 1, 1, true)
+	assertIntersects(t, 2, 2, 2, 0, 1, 1, 3, 1, true)
+	assertIntersects(t, 3, 1, 1, 1, 2, 2, 2, 0, true)
+	assertIntersects(t, 3, 1, 1, 1, 2, 0, 2, 2, true)
+
+	// no overlap
+	assertIntersects(t, 0, 0, 2, 0, 1, 3, 1, 1, false)
+	assertIntersects(t, 2, 0, 0, 0, 1, 3, 1, 1, false)
+}
+
+func TestInside(t *testing.T) {
+	lines := util.SliceAtLine(sampleData)
+	p := parse(lines)
+	tst.Equals(t, true, p.pointIsInside2(3, 4))
+	tst.Equals(t, false, p.pointIsInside2(0, 0))
+	tst.Equals(t, true, p.pointIsInside2(4, 3))
+}
+
+func assertIntersects(t *testing.T, p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y int, intersects bool) {
+	segment1 := &Segment{&Point{p1x, p1y}, &Point{p2x, p2y}}
+	segment2 := &Segment{&Point{p3x, p3y}, &Point{p4x, p4y}}
+	tst.Equals(t, intersects, segmentsIntersect(segment1, segment2))
+}
